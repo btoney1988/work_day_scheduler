84303ccf9459ee9aa4724527f50a02f6f20749bc
Brian Toney <btoney1988@gmail.com>
2020-09-13 10:28:39 -0400

    setting up jquery

--- script.js
+++ script.js
@@ -1,15 +1,84 @@
 $(document).ready(function () {
-  var date = new Date();
-  var time = date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
-  var container = $(".container");
-  var row = $("<div class='row'>");
-  var column = $("<div class='col-12'>");
+  const test = true;
+  // Setting date from user browser
+  var date = moment().format('MMMM Do');
+  var currentDay = $("#currentDay");
+  currentDay.text(date)
+  var dayHour24 = moment().format('H');
+  var dayHour12 = moment().format('h');
+  var saveIcon = "./images/save-regular.svg";
 
-  $("#currentDay").text(date);
+  var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
 
-  console.log(date);
-  console.log(time);
-  container.append(row.text(date));
-  row.append(column.text(time));
+  if (storedPlans !== null) {
+    planTextArr = storedPlans;
+  } else {
+    // this should only occur on first time the app is loaded in the browser
+    planTextArr = new Array(9);
+    planTextArr[4] = "No Plans";
+  }
+  if (test) { console.log("full array of plned text", planTextArr); }
+
+
+
+
+  for (var hour = 9; hour <= 17; hour++) {
+    var planContainer = $('#plannerContainer');
+    // clear existing elements
+    planContainer.empty();
+    // index for array use offset from hour
+    var index = hour - 9;
+    var rowDiv = $("<div class='row plannerRow'>");
+    rowDiv.attr('hour-index', hour);
+
+    var timeDivCol2 = $("<div class='col-2'>");
+
+    var timeBoxDiv = $("<span class='timeBox'>");
+
+    // format hours for display
+    var displayHour = 0;
+    var ampm = "";
+    if (hour > 12) {
+      displayHour = hour - 12;
+      ampm = "pm";
+    } else {
+      displayHour = hour;
+      ampm = "am";
+    }
+
+    timeBoxDiv.text(displayHour + ampm);
+
+    rowDiv.append(timeDivCol2);
+    timeDivCol2.append(timeBoxDiv);
+
+    var planDiv = $("<input>");
+    planDiv.attr('id', `input-${index}`);
+    planDiv.attr('hour-index', index);
+    planDiv.attr('type', 'text');
+    planDiv.attr('class', 'dailyPlan');
+
+    planDiv.val(planTextArr[index]);
+
+    var inputDivCol9 = $("<div class='col-9'>");
+
+    rowDiv.append(inputDivCol9);
+    inputDivCol9.append(planDiv);
+
+    var saveDivCol1 = $("<div class='col-1'>");
+
+    var saveBtn = $("<i>");
+    saveBtn.attr('id', `saveid-${index}`);
+    saveBtn.attr('save-id', index);
+    saveBtn.attr('class', "far fa-save saveIcon");
+
+    rowDiv.append(saveDivCol1);
+    saveDivCol1.append(saveBtn);
+    console.log(rowDiv[0]);
+
+    updateRowColor(rowDiv, hour)
+
+    planContainer.append(rowDiv[0]);
+    console.log(planContainer)
+  }
 
 })
